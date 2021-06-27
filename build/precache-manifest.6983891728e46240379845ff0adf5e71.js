24f53bd0d556355fcebc449b4e824fbad870400a
qiaork <1229319295@qq.com>
2021-06-27 11:55:38 +0800

    add build/

---
+++ build/precache-manifest.6983891728e46240379845ff0adf5e71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bc1f9a083991a3e12b7ac9a4a54d9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1be9921ba4495cd2394c",
+    "url": "./static/css/main.92b44e6e.chunk.css"
+  },
+  {
+    "revision": "e3a1806344f2f22de2cf",
+    "url": "./static/js/2.c5ef3ee6.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "./static/js/2.c5ef3ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be9921ba4495cd2394c",
+    "url": "./static/js/main.6a992d16.chunk.js"
+  },
+  {
+    "revision": "44e39732eb816d1ab058",
+    "url": "./static/js/runtime-main.b2646cd7.js"
+  }
+]);
